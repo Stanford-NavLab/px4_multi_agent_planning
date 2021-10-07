826a4ef8bd95c7104621086bf9a8e3dc7ad0d7c0
Adam Dai <adamdai97@gmail.com>
2021-10-06 17:22:13 -0700

    splitup simple.launch

---
+++ bringup/launch/takeoff.launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+import sys
+
+def generate_launch_description():
+    
+    for arg in sys.argv:
+        if arg.startswith("ns:="):
+            ns = arg.split(":=")[1]
+            sys_id = int(arg.split("_")[1])
+
+    return LaunchDescription([
+        Node(
+            package='offboard_ibqr',
+            node_namespace=ns,
+            node_executable='offboard_control',
+            output='screen',
+            emulate_tty=True
+        )
+    ])
