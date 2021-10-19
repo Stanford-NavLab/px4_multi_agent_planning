9eeb9c3f400ba4ff405b49275e0e70b01efb395f
Exxact <adamdai97@gmail.com>
2021-10-19 16:51:16 -0700

    remove offboard_ibqr submodule

---
+++ offboard_ibqr/src/myTools/TrajectoryPlots.m
@@ -0,0 +1,105 @@
+%% Plots and analysis for iBQR trajectories based on PX4-ROS2 offboard node
+% Author Marco Nunez - maez@alumni.stanford.edu
+
+close all
+clear
+clc
+
+%% Load the trajectory data
+% Each cell contains the vehicle's odometry information for a single trajectory
+% Within each cell, the data is arranged as follows:
+    % columns:
+        % col 1 = timestamp:    microseconds since system start
+        % col 2 = rx:           x-position in NED frame
+        % col 3 = ry:           y-position in NED frame
+        % col 4 = rz:           z-position in NED frame
+        % col 5 = vx:           x-velocity in NED frame
+        % col 6 = vx:           y-velocity in NED frame
+        % col 7 = vx:           z-velocity in NED frame
+        % col 8 = qx:           x-quaternion rotation from FRD body frame to NED frame
+        % col 9 = qy:           y-quaternion rotation from FRD body frame to NED frame
+        % col 10 = qz:          z-quaternion rotation from FRD body frame to NED frame
+        % col 11 = qw:          w-quaternion rotation from FRD body frame to NED frame
+        % col 12 = r:           roll speed in body fixed frame
+        % col 13 = p:           pitch speed in body fixed frame
+        % col 14 = y:           yaw speed in body fixed frame
+    % rows:
+        % number of data points collected
+        
+% position only
+% fName = "logs/Trajectory_Log_2021-08-04-21:09:30.txt";
+
+% position and velocity
+% fName = "logs/Trajectory_Log_2021-08-04-21:13:13.txt";
+% fName = "logs/Trajectory_Log_2021-08-04-21:19:39.txt";
+
+% position, velocity, and acceleration
+fName = "logs/Trajectory_Log_2021-10-13-12-20-33.txt";
+
+% acceleration only
+% fName = "logs/Trajectory_Log_2021-08-04-20:59:17.txt";
+
+trajs = getLogData(fName);
+
+% Load reference trajectory
+ref = importdata("logs/Reference_Trajectory.txt").data(:,:);
+
+% Define and load the column indices for easier readability
+global timestamp rx ry rz vx vy vz qx qy qz qw r p y ax ay az
+loadIndices();
+
+% Call each global variables once so matlab does not squiggle them for being unused...
+timestamp;rx;ry;rz;vx;vy;vz;qx;qy;qz;qw;r;p;y;ax;ay;az;
+
+%% Plot positions for each logged trajectory
+
+fig1 = figure('Renderer', 'painters', 'Position',[50 50 1280 720]);
+blue = [0 0.4470 0.7410];
+orange = [0.8500 0.3250 0.0980];
+
+plot3(ref(:,rx), ref(:,ry), ref(:,rz), '--', 'LineWidth', 2, 'Color', blue);
+hold on
+
+for i=1:length(trajs)
+    % Notice that we are transforming back to ENU coordinates for these plots
+    plot3(trajs{i}(:,ry), trajs{i}(:,rx), -trajs{i}(:,rz), 'LineWidth', 2, 'Color', orange);
+end
+
+title('iBQR Trajectories from Offboard Node','FontSize',20,'FontWeight','b')
+
+xlabel('x-dir [m]','FontSize',20,'FontWeight','b') % x-axis label %
+ylabel('y-dir [m]','FontSize',20,'FontWeight','b') % y-axis label %
+zlabel('z-dir [m]','FontSize',20,'FontWeight','b') % z-axis label %
+legend('Reference', 'Actual (Sim)')
+
+set(gcf,'Color','w') % Set background color to white %
+set(gca,'LineWidth',2,'FontSize',16,'FontWeight','bold') % axis properties
+set(gca,'Color','none')
+grid on
+
+
+%% Plot x-y-z tracking errors
+
+figure(2); hold on
+N = length(ref);
+
+tiledlayout(3,1)
+ax1 = nexttile; ylabel(ax1,'x error');
+ax2 = nexttile; ylabel(ax2,'y error');
+ax3 = nexttile; ylabel(ax3,'z error');
+hold([ax1 ax2 ax3],'on')
+for i=1:length(trajs)
+    % x
+    traj_x = resample(double(trajs{i}(:,ry)),N,length(trajs{i}));
+    plot(ax1,ref(:,rx) - traj_x);
+    % y
+    traj_y = resample(double(trajs{i}(:,rx)),N,length(trajs{i}));
+    plot(ax2,ref(:,ry) - traj_y);
+    % z
+    traj_z = resample(double(-trajs{i}(:,rz)),N,length(trajs{i}));
+    plot(ax3,ref(:,rz) - traj_z);
+end
+
+
+
+
---
+++ offboard_ibqr/src/myTools/trajectory_logger.cpp
@@ -0,0 +1,315 @@
+/**
+ * @brief Trajectory Logger
+ * @file trajectory_logger.cpp
+ * @addtogroup tools
+ * @author Marco Nunez <maez@alumni.stanford.edu>
+ */
+
+/******************************** Include Files *******************************/
+#include <px4_msgs/msg/timesync.hpp>
+#include <px4_msgs/msg/vehicle_odometry.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/char.hpp>
+#include <trajectory_msgs/msg/joint_trajectory.hpp>
+
+#include <cstdint>
+
+#include <chrono>
+#include <time.h>
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <unistd.h>
+
+using namespace std::chrono;
+using namespace std::chrono_literals;
+using namespace px4_msgs::msg;
+using namespace std_msgs::msg;
+using std::placeholders::_1;
+
+/*********************************** Macros ***********************************/
+/* Index values for the x, y, and z quantities in the traj_planned.points array */
+#define X                                   0
+#define Y                                   1
+#define Z                                   2
+#define W                                   3
+
+#define HOME_POSITION_X                     0       // x-coordinate for home
+#define HOME_POSITION_Y                     0       // y-coordinate for home
+#define HOME_POSITION_Z                     -7      // z-coordinate for home
+
+/* Note PX4 uses NED coordinate frame */
+
+#define FILE_NAME_SIZE_OFFSET               32      // This adds some additional space for the file name in order to accomodate the date/time/extension space
+#define HOME_FOLDER_MAX_NAME_SIZE           256     // This determines the size of the "tmp" char array that stores the linux home folder path
+
+#define LOG_FOLDER_PATH_1                   "/px4_ros_com_ros2/src/px4_multi_agent_planning/offboard_ibqr/src/myTools/logs/"
+
+#define LOG_FOLDER_PATH_2                   "/src/px4_multi_agent_planning/offboard_ibqr/src/myTools/logs/"
+
+#define LOG_FILE_NAME                       "Trajectory_Log_%Y-%m-%d-%T.txt"
+#define REF_TRAJ_FILE_NAME                  "Reference_Trajectory.txt"
+
+#define WINDOWS_COMPATIBILITY               1
+
+/************************** Local Function Prototypes *************************/
+
+
+/****************************** Module Variables ******************************/
+
+
+/**************************** OffboardControl Class ***************************/
+class TrajectoryLogger : public rclcpp::Node
+{
+public:
+	//--------- Constructor --------//
+	TrajectoryLogger() : Node("trajectory_logger")
+	{
+		// get common timestamp
+		timesync_sub_ =
+			this->create_subscription<px4_msgs::msg::Timesync>("Timesync_PubSubTopic", 10,
+				[this](const px4_msgs::msg::Timesync::UniquePtr msg) {
+					timestamp_.store(msg->timestamp);
+				});
+
+		// Subscribe to odometry
+		odom_sub_ = this->create_subscription<px4_msgs::msg::VehicleOdometry>("TrajectoryOdometry", 10, std::bind(&TrajectoryLogger::odom_callback, this, _1));
+        // Subscribe to log actions
+        log_action_sub_ = this->create_subscription<std_msgs::msg::Char>("TrajectoryLogAction", 10, std::bind(&TrajectoryLogger::log_action_callback, this, _1));
+        // Subscribe to reference trajectory topic
+        ref_traj_sub_ = this->create_subscription<trajectory_msgs::msg::JointTrajectory>("planner/traj", 10, std::bind(&TrajectoryLogger::ref_trajectory_callback, this, _1));
+
+        // declare a temporary char array to store the path to the linux home folder (256 characters should be sufficient for any computer)
+        char tmp[HOME_FOLDER_MAX_NAME_SIZE];
+        if(!getcwd(tmp, HOME_FOLDER_MAX_NAME_SIZE))
+        {
+            RCLCPP_ERROR(this->get_logger(), "CURRENT DIRECTORY NOT FOUND! ABORT");
+        }
+        else
+        {
+            // Get the path to the linux home folder
+            myPath = tmp;
+            
+            // Determine if the current system includes the /px4_ros_com_ros2 folder as part of the executable's path
+            if (myPath.find("px4_ros_com_ros2") == std::string::npos)
+            {
+                // Add the specific path to the offboard_ibqr log workspace, including the px4_ros_com_ros2 folder
+                myPath += LOG_FOLDER_PATH_1;
+            }
+            else
+            {
+                // Add the specific path to the offboard_ibqr log workspace, excluding the px4_ros_com_ros2 folder
+                myPath += LOG_FOLDER_PATH_2;
+            }
+            
+            // Determine the size to be used for the character array storing the full path name
+            uint16_t nameSizeAllocator = (myPath + LOG_FILE_NAME).size() + FILE_NAME_SIZE_OFFSET;
+            
+            // Allocate memory to store the full file name
+            fileNameBuffer = new char[nameSizeAllocator];
+
+            // Get current time
+            t = time(0);
+            // Store as local time
+            now = localtime(&t);
+            // Write the file name with local time
+            strftime(fileNameBuffer, nameSizeAllocator, (myPath + LOG_FILE_NAME).c_str(), now);
+
+#if WINDOWS_COMPATIBILITY
+            // Modify the time format from HH:MM:SS to HH-MM-SS to meet Microsoft Windows file name requirements
+            uint16_t len = 0;
+            char tmp = fileNameBuffer[0];
+            while (tmp != '\0')
+            {
+                len++;
+                tmp = fileNameBuffer[len];
+            }
+            fileNameBuffer[len-7] = '-';
+            fileNameBuffer[len-10] = '-';
+#endif
+
+            RCLCPP_INFO(this->get_logger(), "%s", fileNameBuffer);
+            // Open the log file
+            logFile.open(fileNameBuffer);
+
+            if(logFile.is_open())
+            {
+                RCLCPP_INFO(this->get_logger(), "Trajectory Log File Opened");
+                // Set the log float precision to 9 to preserve decimal->binary conversion
+                logFile.precision(9);
+                // Set the precision to be for 9 decimal places
+                logFile << std::fixed;
+                // Write the header for the columns
+                logFile << "timestamp " << "rx " << "ry " << "rz " << "vx " << "vy " << "vz " << "qx " << "qy " << "qz " << "qw " << "r " << "p " << "y " << std::endl;
+            }
+            else
+            {
+                RCLCPP_ERROR(this->get_logger(), "Trajectory log file failed to open!");
+            }
+        }
+	}
+
+	//---- Class Public Methods ----//
+
+private:
+	//------ Class Variables -------//
+	rclcpp::Subscription<px4_msgs::msg::Timesync>::SharedPtr timesync_sub_;
+    rclcpp::Subscription<px4_msgs::msg::VehicleOdometry>::SharedPtr odom_sub_;
+    rclcpp::Subscription<std_msgs::msg::Char>::SharedPtr log_action_sub_;
+    rclcpp::Subscription<trajectory_msgs::msg::JointTrajectory>::SharedPtr ref_traj_sub_;
+
+	std::atomic<uint64_t> timestamp_;				//!< common synced timestamped
+
+    mutable std::ofstream refFile;                  // Reference trajectory file
+    mutable std::ofstream logFile;                  // Log file
+    std::time_t t;                                  // Store the current time
+    struct std::tm *now;                            // Store the time as local
+
+    // We use a combination of character array "fileNameBuffer" and string "myPath" to be able to use getcwd() and strftime() functions as well as the
+    // versatility of the std::string operators
+    char *fileNameBuffer;                           // Store the name of the log file with the local time
+    mutable std::string myPath;                     // Store the path to the log folder
+
+	//---- Class Private Methods ----//
+	void odom_callback(const px4_msgs::msg::VehicleOdometry::SharedPtr odom) const;
+    void log_action_callback(const std_msgs::msg::Char::SharedPtr ch) const;
+    void ref_trajectory_callback(const trajectory_msgs::msg::JointTrajectory::SharedPtr ref) const;
+};
+
+/********************* TrajectoryLogger Class Method Definitions *******************/
+/**
+ * @brief Response to TrajectoryOdometry topic. This callback writes the odometry information in the log file
+ */
+void TrajectoryLogger::odom_callback(const px4_msgs::msg::VehicleOdometry::SharedPtr odom) const
+{
+    if(logFile.is_open())
+    {
+        RCLCPP_INFO(this->get_logger(), "writing...");
+        logFile << odom->timestamp << " "
+                << odom->x << " "
+                << odom->y << " "
+                << odom->z << " "
+                << odom->vx << " "
+                << odom->vy << " "
+                << odom->vz << " "
+                << odom->q[X] << " "
+                << odom->q[Y] << " "
+                << odom->q[Z] << " "
+                << odom->q[W] << " "
+                << odom->rollspeed << " "
+                << odom->pitchspeed << " "
+                << odom->yawspeed << std::endl;
+    }
+    else
+    {
+        RCLCPP_ERROR(this->get_logger(), "Trajectory log file is not open!");
+    }
+}
+/**
+ * @brief Response to the TrajectoryLogAction topic. This callback performs any additional action to the log file other than
+ *        writing odometry.
+ */
+void TrajectoryLogger::log_action_callback(const std_msgs::msg::Char::SharedPtr ch) const
+{
+    switch(ch->data)
+    {
+        case 'n':
+        {   // Write a newline to the file
+            if(logFile.is_open())
+            {
+                RCLCPP_INFO(this->get_logger(), "endl");
+                logFile << std::endl;
+            }
+            else
+            {
+                RCLCPP_ERROR(this->get_logger(), "Trajectory log file is not open!");
+            }
+        }
+        break;
+        case 'c':
+        {   // Close the log file
+            logFile.close();
+            RCLCPP_INFO(this->get_logger(), "Trajectory Log File Closed");
+        }
+        break;
+        case 'o':
+        {   // Open the log file
+            logFile.open(fileNameBuffer);
+            if(logFile.is_open())
+            {
+                RCLCPP_INFO(this->get_logger(), "Trajectory Log File Opened");
+            }
+            else
+            {
+                RCLCPP_ERROR(this->get_logger(), "Trajectory log file failed to open!");
+            }
+        }
+        break;
+        default:
+        {
+            RCLCPP_ERROR(this->get_logger(), "Invalid Log Action Command");
+        }
+        break;
+    }
+}
+/**
+ * @brief Response to the planner/traj topic. This callback writes the reference trajectory into a file
+ */
+void TrajectoryLogger::ref_trajectory_callback(const trajectory_msgs::msg::JointTrajectory::SharedPtr ref) const
+{
+    static bool firstPass = true;
+
+    // We only want to store the trajectory the first time it is published (for now)
+    if (firstPass)
+    {
+        // Open and setup the reference trajectory file (we write the 'trunc' option to always overwrite this file)
+        refFile.open((myPath + REF_TRAJ_FILE_NAME).c_str(), std::ofstream::trunc);
+        RCLCPP_INFO(this->get_logger(), "Reference Trajectory Log Opened");
+        // Set the log float precision to 9 to preserve decimal->binary conversion
+        refFile.precision(9);
+        // Set the precision to be for 9 decimal places
+        refFile << std::fixed;
+        // Write the header for the columns
+        refFile << "pad " << "rx " << "ry " << "rz " << "vx " << "vy " << "vz " << "ax " << "ay " << "az " << std::endl;
+
+        for (uint8_t i = 0; i < ref->points[X].positions.size(); i++)
+        {
+            refFile << 0.0  << " "
+                    << ref->points[X].positions[i] + HOME_POSITION_X << " "
+                    << ref->points[Y].positions[i] + HOME_POSITION_Y << " "
+                    << ref->points[Z].positions[i] - HOME_POSITION_Z << " "
+                    << ref->points[X].velocities[i] << " "
+                    << ref->points[Y].velocities[i] << " "
+                    << ref->points[Z].velocities[i] << " "
+                    << ref->points[X].accelerations[i] << " "
+                    << ref->points[Y].accelerations[i] << " "
+                    << ref->points[Z].accelerations[i] << std::endl;
+        }
+
+        refFile.close();
+        RCLCPP_INFO(this->get_logger(), "Reference Trajectory Recorded and Closed");
+        RCLCPP_WARN(this->get_logger(), "VERIFY that the HOME_POSITION macros in the offboard file match this one");
+
+        firstPass = false;
+    }
+}
+
+/********************************* Main Code *********************************/
+int main(int argc, char* argv[])
+{
+	std::cout << "Starting trajectory logger node..." << std::endl;
+	setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+	rclcpp::init(argc, argv);
+	rclcpp::spin(std::make_shared<TrajectoryLogger>());
+
+	rclcpp::shutdown();
+	return 0;
+}
+
+
+/********************************* Footnotes *********************************/
+
+
+/******************************** End of file ********************************/
